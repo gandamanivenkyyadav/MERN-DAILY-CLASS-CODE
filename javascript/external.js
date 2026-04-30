257f459428ddcd0c7b1d0ee0012df53a1e582888
gandamanivenkyyadav <gandamanivenkyyadav@gmail.com>
2026-04-30 10:29:16 +0530

    day-8

---
+++ javascript/external.js
@@ -0,0 +1,21 @@
+num = "java"
+num = 50;
+console.log(num);
+name= true;
+console.log(name);
+
+class smapleExample {
+  display(){
+    console.log("heloo this is example class");
+
+  }
+}
+
+ s1= new smapleExample();
+s1.display();
+s1.test();
+
+function addition() {
+  console.log(10+20)
+}
+addition();
